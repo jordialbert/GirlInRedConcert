a01b92812e7cee7c0e3b826dce227f43159dc64d
Jordi Albert Ribas González <jordialbert@Jordis-MacBook-Pro.local>
2022-01-06 12:59:06 +0100

    Add button to interact with and add animation and handle audio volume to increase it slowly

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
-document.addEventListener('click', () => {
-    init();
-});
+window.onload = () => {
+    const song = new Audio('audio/dot.mp3');
+    const buttons = document.querySelectorAll('[data-destination]');
 
-function init () {
-    let song = new Audio('audio/dot.mp3');
-    song.play();
-    song.muted = false;
+    document.getElementById('1').scrollIntoView({
+        behavior: 'smooth'
+    });
+
+    song.volume = 0.0;
+    song.currentTime = 3;
+
+    buttons.forEach((element) => {
+        element.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            let destination = Number(element.getAttribute('data-destination'));
+            song.play();
+
+            switch (destination) {
+                case 1:
+                    song.volume = 0.0;
+                    song.pause();
+                    song.currentTime = 0;
+                    break;
+
+                case 2:
+                    song.volume = 0.2;
+                    break;
+
+                case 3:
+                    song.volume = 0.4;
+                    break;
+
+                case 4:
+                    song.volume = 0.6;
+                    break;
+
+                case 5:
+                    song.volume = 0.8;
+                    break;
+                case 6:
+                    song.volume = 1.0;
+                    break;
+                default:
+                    break;
+            }
+
+            document.getElementById(`${destination}`).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 }
